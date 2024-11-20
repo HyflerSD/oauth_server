2f8304103629353eb7c6be256d97a572c46fbfa4
joyjodev <joyner.joseph001@mymdc.net>
2024-11-19 20:28:16 -0500

    commit -m

--- src/generator/utils.rs
+++ src/generator/utils.rs
@@ -3,27 +3,33 @@ static NUMERIC: &str = "1234567890";
 static ALPHA_LOWER: &str = "abcdefghijklmnopqrstuvwxyz";
 static ALPHA_UPPER: &str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 static ALPHA: &str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const PREFIX: &str = "1000";
-
+static PREFIX: &str = "1000.";
+static SEGMENT_LEN: usize = 24;
+static SEPERATORS: usize = 2;
 pub fn generate_client_id() -> String {
     //just temp code bro
-    generate(12, ALPHANUMERIC)
+    generate(ALPHA_LOWER)
 } 
 
 
-pub fn generate<S: AsRef<str>>(length: usize, charset: S) -> String {
+pub fn generate<S: AsRef<str>>(charset: S) -> String {
     let charset_str = charset.as_ref();
     assert_eq!(charset_str.is_empty(), false);
-    
-    let chars: Vec<char> = charset_str.chars().collect();
-    let mut res = String::with_capacity(length);
 
+    let chars: Vec<char> = charset_str.chars().collect();
+    let mut res = String::with_capacity((SEGMENT_LEN * SEPERATORS) +  SEPERATORS - 1);
     unsafe {
-        for _ in 0..length {
-            res.push(
-                *chars.get_unchecked(fastrand::usize(0..chars.len()))
-            );
+        for c in 1..=SEPERATORS {
+            for i in 0..SEGMENT_LEN {
+                res.push(
+                    *chars.get_unchecked(fastrand::usize(0..chars.len()))
+                );
+            }
+
+            if c < SEPERATORS {
+                res.push('.');
+            }
         }
     }
-    res
+    PREFIX.to_owned() + &res
 }
