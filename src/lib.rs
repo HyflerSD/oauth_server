ac54cb61fc3100037dda0645e3958e7de4e7e75a
michael <michael@peoplelikesoftware.com>
2024-11-20 23:38:15 -0500

    validate

--- src/lib.rs
+++ src/lib.rs
@@ -5,18 +5,21 @@ use hyper::body::Bytes;
 
 pub mod generator;
 
+
+
 pub fn run(st: &str) {
     println!("Your code is: {}", st);
 }
 
 
 pub async fn handle_client(req: Request<impl hyper::body::Body + std::fmt::Debug>) -> Result<Response<Full<Bytes>>, Infallible> {
+
+    println!("{}", validate_req(&req));
+
     match req.method() {
         &Method::GET => {
-            //println!("Body: {:?}", req.body());
-            let (parts, body) = req.into_parts();
-            println!("{:?}", parts.uri);
         }
+
         &Method::POST => {
             println!("This is a post")
         }
@@ -35,4 +38,39 @@ pub async fn handle_client(req: Request<impl hyper::body::Body + std::fmt::Debug
     let st = generator::utils::generate_client_id();
     run(&st[..]);
     Ok(Response::new(Full::new(Bytes::from("Here i am"))))
-} 
+}
+
+pub fn validate_req(req: &Request<impl hyper::body::Body>) -> bool {
+    if Endpoint::variants().iter().any(|endpoint| req.uri().path() == endpoint.as_str()) {
+        return true;
+    }
+    false
+}
+
+
+enum Endpoint {
+    Token,
+    Auth,
+    Revoke
+}
+
+impl Endpoint {
+    fn as_str(&self) -> &'static str {
+        match self {
+            Endpoint::Token => "/oauth/v2/token",
+            Endpoint::Auth => "/oauth/v2/auth",
+            Endpoint::Revoke => "/oauth/v2/token/revoke",
+
+        }
+    }
+
+    fn variants() -> &'static [Endpoint] {
+        static VARIANTS: &[Endpoint] = &[
+            Endpoint::Token,
+            Endpoint::Auth,
+            Endpoint::Revoke
+        ];
+        VARIANTS
+    }
+}
+
